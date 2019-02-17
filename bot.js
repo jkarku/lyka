fad600744e721d715bb16d7e8658f44c78e7a479
jkarku <47725311+jkarku@users.noreply.github.com>
2019-02-18 01:10:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('message', message => {
         switch(cmd) {
             // !ping
             case 'ping':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'Burn! :fire:'
 
@@ -24,63 +24,63 @@ client.on('message', message => {
             break;
             // !help
             case 'help':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'I work with commands: !zak !recruit !members !jr !invite !event !boss !RR'
 		            });
             break;
             // !recruit
             case 'recruit':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'New application: https://mapleroyals.com/forum/threads/resonance-just-beat-it.131324/page-2'
                 });
             break;
             // !members
             case 'members':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'List of current members can be found in here #members'
                 });
             break;
             // !jr
             case 'jr':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'Call for aid. Better tag all of them @Maestro'
                 });
 	          break;
          // !boss
             case 'boss':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: '_"The one leader to rule them all..._" @Virtuoso Jake, HIDE THE BOTTLES!'
                 });
 	          break;
          // !event
             case 'event':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: '_No events can be found..._ Make one! #events'
                 });
 	          break;
          // !RR
             case 'RR':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'Lost Ronald due to too many name changes? I got you: @///////#0151'
                 });
 	         break;
             // !zak
             case 'zak':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'ZAK TIME. Running in 15 minutes join if you can'
 		            });
 	         break;
             // !invite
             case 'invite':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'Send this link to your friend: https://discord.gg/dvpTMzq'
 		            });

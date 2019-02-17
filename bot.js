55d668cb093f7e4529c2b785a7e7b9a50a0eb733
jkarku <47725311+jkarku@users.noreply.github.com>
2019-02-18 01:14:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,71 +19,102 @@ client.on('message', message => {
                     to: channelID,
                     message: 'Burn! :fire:'
 
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: (process.env.BOT_TOKEN),
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Burn! :fire:'
+
 
                 });
             break;
             // !help
             case 'help':
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: 'I work with commands: !zak !recruit !members !jr !invite !event !boss !RR'
-		            });
+		  });
             break;
             // !recruit
             case 'recruit':
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: 'New application: https://mapleroyals.com/forum/threads/resonance-just-beat-it.131324/page-2'
                 });
             break;
             // !members
             case 'members':
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: 'List of current members can be found in here #members'
                 });
             break;
             // !jr
             case 'jr':
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: 'Call for aid. Better tag all of them @Maestro'
                 });
-	          break;
+	  break;
          // !boss
             case 'boss':
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: '_"The one leader to rule them all..._" @Virtuoso Jake, HIDE THE BOTTLES!'
                 });
-	          break;
+	  break;
          // !event
             case 'event':
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: '_No events can be found..._ Make one! #events'
                 });
-	          break;
+	break;
          // !RR
             case 'RR':
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: 'Lost Ronald due to too many name changes? I got you: @///////#0151'
                 });
 	         break;
             // !zak
             case 'zak':
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: 'ZAK TIME. Running in 15 minutes join if you can'
-		            });
+		  });
 	         break;
             // !invite
             case 'invite':
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: 'Send this link to your friend: https://discord.gg/dvpTMzq'
-		            });
+		  });
 
 
 

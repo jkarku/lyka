9bb3462c1769e96762694b47660f0d6a8be3f771
jkarku <47725311+jkarku@users.noreply.github.com>
2019-02-18 01:19:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,14 @@
 
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const bot = new Discord.Client({
-   token: (process.env.TOKEN),
-   autorun: true
 
 client.on('ready', () => {
 	console.log('I am ready!');
 });
 
-client.on('message', message => {
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];

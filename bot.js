1df2d375800e36489b71d75037fd7bcb3d9aa6f1
jkarku <47725311+jkarku@users.noreply.github.com>
2019-02-18 01:16:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,43 +1,15 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const bot = new Discord.Client({
+   token: (process.env.TOKEN),
+   autorun: true
 
 client.on('ready', () => {
 	console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                client.sendMessage({
-                    to: channelID,
-                    message: 'Burn! :fire:'
-
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: (process.env.BOT_TOKEN),
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
@@ -58,7 +30,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 bot.sendMessage({
                     to: channelID,
                     message: 'I work with commands: !zak !recruit !members !jr !invite !event !boss !RR'
-		  });
+		            });
             break;
             // !recruit
             case 'recruit':
@@ -80,21 +52,21 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: 'Call for aid. Better tag all of them @Maestro'
                 });
-	  break;
+	          break;
          // !boss
             case 'boss':
                 bot.sendMessage({
                     to: channelID,
                     message: '_"The one leader to rule them all..._" @Virtuoso Jake, HIDE THE BOTTLES!'
                 });
-	  break;
+	          break;
          // !event
             case 'event':
                 bot.sendMessage({
                     to: channelID,
                     message: '_No events can be found..._ Make one! #events'
                 });
-	break;
+	          break;
          // !RR
             case 'RR':
                 bot.sendMessage({
@@ -107,14 +79,14 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 bot.sendMessage({
                     to: channelID,
                     message: 'ZAK TIME. Running in 15 minutes join if you can'
-		  });
+		            });
 	         break;
             // !invite
             case 'invite':
                 bot.sendMessage({
                     to: channelID,
                     message: 'Send this link to your friend: https://discord.gg/dvpTMzq'
-		  });
+		            });
 
 
 
@@ -123,3 +95,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 });
 
 client.login(process.env.BOT_TOKEN);
+
+
+
+
